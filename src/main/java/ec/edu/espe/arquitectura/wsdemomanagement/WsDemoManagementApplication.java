346171c720e0db683424b7becae343aecac6cad9
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-08-22 19:10:28 -0500

    Spring Initializr

---
+++ src/main/java/ec/edu/espe/arquitectura/wsdemomanagement/WsDemoManagementApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.wsdemomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsDemoManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsDemoManagementApplication.class, args);
+    }
+
+}
